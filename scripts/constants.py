ec5a7d14589f51035a4bd5c41cf0e1792e08844d
etm49 <etm49@cornell.edu>
2022-07-22 19:41:48 +0300

    update: relative paths

--- scripts/constants.py
+++ scripts/constants.py
@@ -2,19 +2,19 @@
 
 # ML metadata json path
 #mlMetadata = "/Volumes/My Passport for Mac/jobs/UNDP/ML Interface/automate test/Automate/data/ml/ML Model Metadata.json"
-mlMetadata = "/Volumes/My Passport for Mac/jobs/UNDP/ML Interface/automate test/Automate/data/api/data/ml/ML Model Metadata.json"
+mlMetadata = "../data/api/data/ml/ML Model Metadata.json"
 # Country-level indicator Data and Meta data path
 DATASETS_PATH = "/Volumes/My Passport for Mac/jobs/UNDP/ML-IndicatorData/"
 
 # ML folder in API path (where final json output should be stored)
 #savepath = "/Volumes/My Passport for Mac/jobs/UNDP/ML Interface/automate test/Automate/data/ml/"
-savepath = "/Volumes/My Passport for Mac/jobs/UNDP/ML Interface/automate test/Automate/data/api/data/ml/"
+savepath = "../data/api/data/ml/"
 # Intermediate (raw model output) result storage path
-mlResults = "/Volumes/My Passport for Mac/jobs/UNDP/ML Interface/automate test/Automate/mlResults/"
+mlResults = "../mlResults/"
 
 # API repo path
 #PATH_OF_GIT_REPO ="/Volumes/My Passport for Mac/jobs/UNDP/ML Interface/automate test/Automate"# "../api" # make sure .git folder is properly configured
-PATH_OF_GIT_REPO ="/Volumes/My Passport for Mac/jobs/UNDP/ML Interface/automate test/Automate/data/api"
+PATH_OF_GIT_REPO ="../data/api"
 
 SIDS = ['ASM', 'AIA', 'ATG', 'ABW', 'BHS', 'BRB', 'BLZ', 'BES', 'VGB', 'CPV', 'COM', 'COK', 'CUB', 'CUW', 'DMA', 'DOM',
         'FJI', 'PYF',
